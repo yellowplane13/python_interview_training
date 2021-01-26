a77de3ad54a5cbe6cbce52faf0c983f513f71697
Swathi <bharatbeltur@gmail.com>
2021-01-25 19:42:00 -0500

    randomly add integers to BST

--- treesAndGraphs/binarySearchTree.py
+++ treesAndGraphs/binarySearchTree.py
@@ -1,9 +1,12 @@
-#https://www.geeksforgeeks.org/binary-search-tree-set-1-search-and-insertion/
+# https://www.geeksforgeeks.org/binary-search-tree-set-1-search-and-insertion/
+import random
+
+
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.left = None
-        self.right = None 
+        self.right = None
 
 #           5
 #         /   \
@@ -11,131 +14,61 @@ class Node:
 #      / \     /  \
 #     2   4   6    8
 # insert(5,5.5)
-#insert(4,3)
+# insert(4,3)
 
-def insert(root, data):
-    if root is None:
-        root = Node(data)
-        return
 
-    if root.data == data:
-        return root
-    
-    elif root.data < data:
-        if root.right is None:
-            root.right = Node(data)
-        else:
+class Tree:
+    def insert(self, root, data):
+        if root is None:
+            root = Node(data)
+            return
+
+        if root.data == data:
+            return root
+
+        elif root.data < data:
+            if root.right is None:
+                root.right = Node(data)
+            else:
             # if root.right.data < data:
             #     insert(root.right, data)
             # else:
-            insert(root.right,data)
-    
-    else:
-        # if root.data > data:
-        if root.left is None:
-            root.left = Node(data)
-            return
+                self.insert(root.right, data)
+
         else:
+        # if root.data > data:
+            if root.left is None:
+                root.left = Node(data)
+                return
+            else:
             # if root.left.data > data:
             #     insert(root.left, data)
             # else:
-            insert(root.left,data)
-    
+                self.insert(root.left, data)
 
-def preOrder(root):
-    if root is not None:
-        print(root.data)
-        preOrder(root.left)
-        preOrder(root.right)
-
-tree = Node(5)
-insert(tree,3)
-insert(tree,4)
-insert(tree,7)
-insert(tree,6)
-insert(tree,8)
-
-preOrder(tree)
+    def inOrder(self, root):
+        if root is not None:
+            self.inOrder(root.left)
+            print(root.data)
+            self.inOrder(root.right)
 
 
+if __name__ == '__main__':
+    tree = Tree()
+    root = Node(random.randint(0,100))
+    print(root.data,":root's val")
+    for i in range(10):
+        n = random.randint(0,200)
+        print("num getting inserted:",n)
+        tree.insert(root,n)
+    tree.inOrder(root)
 
 
+# tree = Node(5)
+# insert(tree, 3)
+# insert(tree, 4)
+# insert(tree, 7)
+# insert(tree, 6)
+# insert(tree, 8)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class BinarySearchTree:
-#     # Initialize an empty tree
-#     def __init__(self):
-#         self.root = None
-        
-#     # we do the insert recursively
-#     # check if self.root exists, if it does, search down tree from there recursively 
-#     # using private _insert function to do the recurisive calls
-#     def insert(self, data):
-#         if self.root is None:
-#             self.root = Node(data)
-#         else:
-#             self._insert(data, self.root)
-
-#     def _insert(self,data,currNode):
-#         if data < currNode.data:
-#             if currNode.left != None:
-#                 self._insert(data,currNode.left)
-#             else:
-#                 currNode.left = Node(data)
-        
-#         elif data > currNode.data:
-#             if currNode.right != None:
-#                 self._insert(data,currNode.right)
-#             else:
-#                 currNode.right = Node(data)
-#         else:
-#             print(f"Current data {data} passed already in the tree")
-
-#     def printList(self):
-#         if self.root is None:
-#             print("the tree is empty")
-#         else:
-#             ("getting into _print function")
-#             #print(self.root.data)
-#             self._printList(self.root)
-    
-#     def _printList(self, currNode):
-#         if currNode.left != None:
-#             print("the value to be inserted is : ", currNode.left.data)
-#             self._printList(currNode.left)
-#             print(str(currNode.data))
-#             self._printList(currNode.right)
-#         #     print(currNode.data)
-#         #     self._printList(currNode.left)
-#         # elif currNode.right != None:
-#         #     print(currNode.data)
-#         #     self._printList(currNode.right)
-
-#     def fillTree(self, tree, n = 10, maxInt = 20):
-#         from random import randint
-#         for _ in range(0,n):
-#             currElement = randint(0,maxInt)
-#             tree.insert(currElement)
-#             print("the element getting iserted into tree", currElement)
-#         return tree
-    
-# tree = BinarySearchTree()
-# tree = tree.fillTree(tree)
-# tree.printList()
+# preOrder(tree)
